b4fbac6773c33d57cd1fc1f05269aff5deae1d05
Ahmed22Ayman <ahmed2107685@miuegypt.edu.eg>
2024-12-04 16:16:55 +0200

    login, pubspec

--- mobile_project/lib/features/screens/login/login.dart
+++ mobile_project/lib/features/screens/login/login.dart
@@ -2,13 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:mobile_project/common/styles/spacing_styles.dart';
 import 'package:mobile_project/utils/constants/image_setting.dart';
 import 'package:mobile_project/utils/constants/sizes.dart';
+import 'package:mobile_project/utils/constants/text_strings.dart';
+import 'package:mobile_project/utils/helpers/helper_function.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    final dark = THelperFunctions.isDarkMode(context);
+
+    return Scaffold(
       body: SingleChildScrollView(
         child: Padding(
           padding: TSpacingStyles.paddingWithAppBarHeight,
@@ -19,8 +23,14 @@ class LoginScreen extends StatelessWidget {
                 children: [
                   Image(
                     height: 150,
-                    image: AssetImage(TImages.darkAppLogo),
-                  )
+                    image: AssetImage(
+                        dark ? TImages.lightAppLogo : TImages.darkAppLogo),
+                  ),
+                  Text(TTexts.loginTitle,
+                      style: Theme.of(context).textTheme.headlineMedium),
+                  const SizedBox(height: TSizes.sm),
+                  Text(TTexts.loginSubTitle,
+                      style: Theme.of(context).textTheme.bodyMedium),
                 ],
               )
             ],
