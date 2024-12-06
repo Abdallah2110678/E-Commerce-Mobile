aad529b08492a01a24949dbd87be51f8c5a62e58
Ahmed22Ayman <ahmed2107685@miuegypt.edu.eg>
2024-12-06 16:08:10 +0200

    login

--- mobile_project/lib/features/screens/login/login.dart
+++ mobile_project/lib/features/screens/login/login.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:iconsax/iconsax.dart';
 import 'package:mobile_project/common/styles/spacing_styles.dart';
+import 'package:mobile_project/utils/constants/colors.dart';
 import 'package:mobile_project/utils/constants/image_setting.dart';
 import 'package:mobile_project/utils/constants/sizes.dart';
 import 'package:mobile_project/utils/constants/text_strings.dart';
@@ -18,6 +21,7 @@ class LoginScreen extends StatelessWidget {
           padding: TSpacingStyles.paddingWithAppBarHeight,
           child: Column(
             children: [
+              /////logo, title
               Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
@@ -32,7 +36,126 @@ class LoginScreen extends StatelessWidget {
                   Text(TTexts.loginSubTitle,
                       style: Theme.of(context).textTheme.bodyMedium),
                 ],
-              )
+              ),
+              ////form
+              Form(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                      vertical: TSizes.spaceBtwSections),
+                  child: Column(
+                    children: [
+                      ///Email
+                      TextFormField(
+                        decoration: const InputDecoration(
+                            prefixIcon: Icon(Iconsax.direct_right),
+                            labelText: TTexts.email),
+                      ),
+                      const SizedBox(height: TSizes.spaceBtwInputFields),
+
+                      ///Password
+                      TextFormField(
+                        decoration: const InputDecoration(
+                            prefixIcon: Icon(Iconsax.password_check),
+                            labelText: TTexts.password,
+                            suffixIcon: Icon(Iconsax.eye_slash)),
+                      ),
+                      const SizedBox(height: TSizes.spaceBtwInputFields / 2),
+
+                      ///Remember me & forget password
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          //remember me
+                          Row(
+                            children: [
+                              Checkbox(value: true, onChanged: (Value) {}),
+                              const Text(TTexts.rememberMe),
+                            ],
+                          ),
+                          //forget password
+                          TextButton(
+                            onPressed: () {},
+                            child: const Text(TTexts.forgetPassword),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: TSizes.spaceBtwSections),
+
+                      ///sign in button
+                      SizedBox(
+                          width: double.infinity,
+                          child: ElevatedButton(
+                              onPressed: () {},
+                              child: const Text(TTexts.signIn))),
+                      const SizedBox(height: TSizes.spaceBtwItems),
+
+                      ///create account button
+                      SizedBox(
+                          width: double.infinity,
+                          child: OutlinedButton(
+                              onPressed: () {},
+                              child: const Text(TTexts.createAccount))),
+                    ],
+                  ),
+                ),
+              ),
+              ////divider
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Flexible(
+                    child: Divider(
+                        color: dark ? TColors.darkGrey : TColors.grey,
+                        thickness: 0.5,
+                        indent: 60,
+                        endIndent: 5),
+                  ),
+                  Text(TTexts.orSignInWith.capitalize!,
+                      style: Theme.of(context).textTheme.labelMedium),
+                  Flexible(
+                    child: Divider(
+                        color: dark ? TColors.darkGrey : TColors.grey,
+                        thickness: 0.5,
+                        indent: 5,
+                        endIndent: 60),
+                  ),
+                ],
+              ),
+              const SizedBox(width: TSizes.spaceBtwSections),
+
+              ///footer
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                        border: Border.all(color: TColors.grey),
+                        borderRadius: BorderRadius.circular(100)),
+                    child: IconButton(
+                      onPressed: () {},
+                      icon: const Image(
+                        width: TSizes.iconMd,
+                        height: TSizes.iconMd,
+                        image: AssetImage(TImages.google),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: TSizes.spaceBtwItems),
+                  Container(
+                    decoration: BoxDecoration(
+                        border: Border.all(color: TColors.grey),
+                        borderRadius: BorderRadius.circular(100)),
+                    child: IconButton(
+                      onPressed: () {},
+                      icon: const Image(
+                        width: TSizes.iconMd,
+                        height: TSizes.iconMd,
+                        image: AssetImage(TImages.facebook),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ],
           ),
         ),
--- mobile_project/lib/utils/constants/image_setting.dart
+++ mobile_project/lib/utils/constants/image_setting.dart
@@ -1,17 +1,16 @@
 class TImages {
   static const String darkAppLogo =
-      "mobile_project/assets/logos/t-store-splash-logo-black.png";
+      "assets/logos/t-store-splash-logo-black.png";
   static const String lightAppLogo =
-      "mobile_project/assets/logos/t-store-splash-logo-white.png";
+      "assets/logos/t-store-splash-logo-white.png";
 
-  static const String google = "mobile_project/assets/logos/google-icon.png";
-  static const String facebook =
-      "mobile_project/assets/logos/facebook-icon.png";
+  static const String google = "assets/logos/google-icon.png";
+  static const String facebook = "assets/logos/facebook-icon.png";
 
   static const String onBoardingImage1 =
-      "mobile_project/assets/images/on_boarding_images/sammy-line-searching.gif";
+      "assets/images/on_boarding_images/sammy-line-searching.gif";
   static const String onBoardingImage2 =
-      "mobile_project/assets/images/on_boarding_images/sammy-line-shopping.gif";
+      "assets/images/on_boarding_images/sammy-line-shopping.gif";
   static const String onBoardingImage3 =
-      "mobile_project/assets/images/on_boarding_images/sammy-line-delivery.gif";
+      "assets/images/on_boarding_images/sammy-line-delivery.gif";
 }
