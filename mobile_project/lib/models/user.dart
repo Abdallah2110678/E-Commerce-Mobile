dbdd5964f6bbae70d389a4ea8400298ff4c8ba84
Abdallah Hamdy <96912938+Abdallah2110678@users.noreply.github.com>
2024-12-08 10:55:06 +0200

    edited something

--- mobile_project/lib/models/user.dart
+++ mobile_project/lib/models/user.dart
@@ -1,6 +1,29 @@
 class User {
+  final String id;
+  String name;
   String email;
-  String password;
 
-  User({required this.email, required this.password});
+  User({
+    required this.id,
+    required this.name,
+    required this.email,
+  });
+
+  // Convert JSON to User
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      id: json['id'],
+      name: json['name'],
+      email: json['email'],
+    );
+  }
+
+  // Convert User to JSON
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'email': email,
+    };
+  }
 }
