bc153913f8e068105fabec3184be16ea138adeb2
Ahmed22Ayman <ahmed2107685@miuegypt.edu.eg>
2024-12-15 12:26:27 +0200

    editing the home screen

--- mobile_project/lib/views/home/home.dart
+++ mobile_project/lib/views/home/home.dart
@@ -1,10 +1,139 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_project/utils/constants/colors.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            //curved widget
+            TPrimaryHeaderContainer(
+              child: Container(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+///circular container
+class TCircularContainer extends StatelessWidget {
+  const TCircularContainer(
+      {super.key,
+      this.width = 400,
+      this.height = 400,
+      this.radius = 400,
+      this.padding = 0,
+      this.child,
+      this.backgroundColor = TColors.white});
+
+  final double? width;
+  final double? height;
+  final double radius;
+  final double padding;
+  final Widget? child;
+  final Color backgroundColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      height: height,
+      padding: EdgeInsets.all(padding),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(radius),
+        color: backgroundColor,
+      ),
+      child: child,
+    );
+  }
+}
+
+///curve edget
+class TCustomCurvedEdges extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+    path.lineTo(0, size.height);
+
+    final firstCurve = Offset(0, size.height - 20);
+    final lastCurve = Offset(30, size.height - 20);
+    path.quadraticBezierTo(
+        firstCurve.dx, firstCurve.dy, lastCurve.dx, lastCurve.dy);
+
+    final secondFirstCurve = Offset(0, size.height - 20);
+    final secondLastCurve = Offset(size.width - 30, size.height - 20);
+    path.quadraticBezierTo(secondFirstCurve.dx, secondFirstCurve.dy,
+        secondLastCurve.dx, secondLastCurve.dy);
+
+    final thirdFirstCurve = Offset(size.width, size.height - 20);
+    final thirdLastCurve = Offset(size.width, size.height);
+    path.quadraticBezierTo(thirdFirstCurve.dx, thirdFirstCurve.dy,
+        thirdLastCurve.dx, thirdLastCurve.dy);
+
+    path.lineTo(size.width, 0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
+    return true;
+  }
+}
+
+///widge curve edged
+class TCurvedEdgeWidget extends StatelessWidget {
+  const TCurvedEdgeWidget({super.key, this.child});
+
+  final Widget? child;
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipPath(
+      clipper: TCustomCurvedEdges(),
+      child: child,
+    );
+  }
+}
+
+///header container
+class TPrimaryHeaderContainer extends StatelessWidget {
+  const TPrimaryHeaderContainer({super.key, required this.child});
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return TCurvedEdgeWidget(
+      child: Container(
+        color: TColors.primary,
+        padding: const EdgeInsets.all(0),
+        child: SizedBox(
+          height: 400,
+          child: Stack(
+            children: [
+              Positioned(
+                top: -150,
+                right: -250,
+                child: TCircularContainer(
+                    backgroundColor: TColors.textWhite.withOpacity(0.1)),
+              ),
+              Positioned(
+                top: 100,
+                right: -300,
+                child: TCircularContainer(
+                    backgroundColor: TColors.textWhite.withOpacity(0.1)),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
