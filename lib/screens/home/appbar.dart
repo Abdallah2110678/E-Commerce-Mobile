1cee05e3c9268c697e4bad28d804beed3cb13e59
Ahmed22Ayman <ahmed2107685@miuegypt.edu.eg>
2025-01-20 23:11:49 +0200

    make the arrow work for the dark mode

--- lib/screens/home/appbar.dart
+++ lib/screens/home/appbar.dart
@@ -5,13 +5,14 @@ import 'package:mobile_project/utils/constants/sizes.dart';
 import 'package:mobile_project/utils/device/device_utility.dart';
 
 class TAppbar extends StatelessWidget implements PreferredSizeWidget {
-  const TAppbar(
-      {super.key,
-      this.title,
-      this.showBlackArrow = true,
-      this.leadingIcon,
-      this.actions,
-      this.leadingOnPressed});
+  const TAppbar({
+    super.key,
+    this.title,
+    this.showBlackArrow = true,
+    this.leadingIcon,
+    this.actions,
+    this.leadingOnPressed,
+  });
 
   final Widget? title;
   final bool showBlackArrow;
@@ -21,6 +22,11 @@ class TAppbar extends StatelessWidget implements PreferredSizeWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Determine the icon color based on the theme (dark or light mode)
+    final iconColor = Theme.of(context).brightness == Brightness.dark
+        ? Colors.white // Use white for dark mode
+        : Colors.black; // Use black for light mode
+
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: TSizes.md),
       child: AppBar(
@@ -28,10 +34,14 @@ class TAppbar extends StatelessWidget implements PreferredSizeWidget {
         leading: showBlackArrow
             ? IconButton(
                 onPressed: () => Get.back(),
-                icon: const Icon(Iconsax.arrow_left))
+                icon: Icon(Iconsax.arrow_left,
+                    color: iconColor), // Set icon color
+              )
             : leadingIcon != null
                 ? IconButton(
-                    onPressed: leadingOnPressed, icon: Icon(leadingIcon))
+                    onPressed: leadingOnPressed,
+                    icon: Icon(leadingIcon, color: iconColor), // Set icon color
+                  )
                 : null,
         title: title,
         actions: actions,
