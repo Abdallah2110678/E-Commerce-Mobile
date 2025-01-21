8c12eddc9ba1c507abecc4ad84155dc68f4d6ca1
Abdallah Hamdy <96912938+Abdallah2110678@users.noreply.github.com>
2025-01-21 10:42:53 +0200

    fix something

--- lib/screens/dashboard/dashboard.dart
+++ lib/screens/dashboard/dashboard.dart
@@ -46,7 +46,7 @@ class _DashboardState extends State<Dashboard> {
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Text(
-                            'Welcome Back ${userController.user.value.firstName}',
+                            'Welcome ${userController.user.value.fullName}',
                             style: GoogleFonts.poppins(
                                 color: Colors.white,
                                 fontSize: 20,
